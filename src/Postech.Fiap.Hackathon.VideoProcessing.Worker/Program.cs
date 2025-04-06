18b06b72e9fcaf71e338991ba504f26e9ff85c7f
Alexandre Nascimento <alexandre.n@outlook.com>
2025-04-06 14:26:44 -0300

    Initialize video processing worker project with Docker support
    Add the initial setup for the VideoProcessing.Worker project, including appsettings, Docker configuration, Dockerfile, and solution file. Configures launch settings, introduces a .dockerignore file, and sets up logging, environment variables, and OpenAPI dependencies.

---
+++ src/Postech.Fiap.Hackathon.VideoProcessing.Worker/Program.cs
@@ -0,0 +1,13 @@
+namespace Postech.Fiap.Hackathon.VideoProcessing.Worker;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        app.Run();
+    }
+}
