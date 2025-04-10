c82d52808dd5d71820a02501bd972d2d44e041a6
Alexandre Nascimento <alexandre.n@outlook.com>
2025-04-09 21:58:19 -0300

    Refactor video processing command handling logic.
    Replaced direct database context usage with a repository pattern for better abstraction and maintainability. Enhanced video processing logic to include proper status updates, error handling, and detailed logging for each step of the process. Removed unused `VideoProcessorResponse` class to streamline the command response.

--- src/Postech.Fiap.Hackathon.VideoProcessing.Worker/Features/Videos/VideoProcessor/Commands/VideoProcessorCommand.cs
+++ src/Postech.Fiap.Hackathon.VideoProcessing.Worker/Features/Videos/VideoProcessor/Commands/VideoProcessorCommand.cs
@@ -2,8 +2,4 @@ using Postech.Fiap.Hackathon.VideoProcessing.Worker.Common.ResultPattern;
 
 namespace Postech.Fiap.Hackathon.VideoProcessing.Worker.Features.Videos.VideoProcessor.Commands;
 
-public record VideoProcessorCommand(string VideoId) : IRequest<Result<VideoProcessorResponse>>;
-
-public class VideoProcessorResponse
-{
-}
+public record VideoProcessorCommand(string VideoId) : IRequest<Result>;
--- src/Postech.Fiap.Hackathon.VideoProcessing.Worker/Features/Videos/VideoProcessor/Commands/VideoProcessorCommandHandler.cs
+++ src/Postech.Fiap.Hackathon.VideoProcessing.Worker/Features/Videos/VideoProcessor/Commands/VideoProcessorCommandHandler.cs
@@ -1,41 +1,81 @@
-using Microsoft.EntityFrameworkCore;
 using Postech.Fiap.Hackathon.VideoProcessing.Worker.Common.ResultPattern;
+using Postech.Fiap.Hackathon.VideoProcessing.Worker.Features.Videos.Models;
 using Postech.Fiap.Hackathon.VideoProcessing.Worker.Features.Videos.VideoProcessor.Interfaces;
-using Postech.Fiap.Hackathon.VideoProcessing.Worker.Persistence;
 
 namespace Postech.Fiap.Hackathon.VideoProcessing.Worker.Features.Videos.VideoProcessor.Commands;
 
 public class VideoProcessorCommandHandler(
-    ApplicationDbContext context,
-    IVideoDownloader videoDownloader,
+    IVideoRepository videoRepository,
+    IStorageService storageService,
+    IVideoFrameExtractor videoFrameExtractor,
     ILogger<VideoProcessorCommandHandler> logger)
-    : IRequestHandler<VideoProcessorCommand, Result<VideoProcessorResponse>>
+    : IRequestHandler<VideoProcessorCommand, Result>
 {
-    public async Task<Result<VideoProcessorResponse>> Handle(VideoProcessorCommand request,
-        CancellationToken cancellationToken)
+    public async Task<Result> Handle(VideoProcessorCommand request, CancellationToken cancellationToken)
     {
-        var video = await context.Videos.FirstOrDefaultAsync
-        (
-            v => v.Id == Guid.Parse(request.VideoId),
-            cancellationToken: cancellationToken
-        );
-
-        if (video == null)
+        try
         {
-            return Result.Failure<VideoProcessorResponse>(Error.Failure("VideoProcessorCommandHandler.Handle",
-                $"Video with ID {request.VideoId} not found."));
+            var video = await videoRepository.GetVideoByIDAsync(request.VideoId, cancellationToken);
+
+            if (video is null)
+                return Result.Failure(Error.Failure("VideoProcessorCommandHandler.Handle",
+                    $"Vídeo com o ID {request.VideoId} não encontrado."));
+
+            if (video.Status != VideoStatus.Uploaded)
+                return Result.Failure(Error.Failure("VideoProcessorCommandHandler.Handle",
+                    $"O vídeo com o ID {video.Id} não está em um estado válido para processamento."));
+
+            await videoRepository.ChangeStatusAsync(video.Id.ToString(), VideoStatus.Processing, cancellationToken);
+            logger.LogInformation("Status do vídeo alterado para 'Processando'. ID: {VideoId}", video.Id);
+
+            var videoDownloadResult = await storageService.DownloadAsync(
+                video.Id,
+                video.FilePath,
+                cancellationToken);
+            if (videoDownloadResult.IsFailure)
+            {
+                logger.LogError("Erro ao baixar o vídeo com ID: {VideoId}. Motivo: {Error}", video.Id, videoDownloadResult.Error.Message);
+                return Result.Failure(videoDownloadResult.Error);
+            }
+
+            logger.LogInformation("Vídeo baixado com sucesso. ID: {VideoId}", video.Id);
+
+            var framesResult = await videoFrameExtractor.ExtractFramesAsync(
+                video.Id,
+                videoDownloadResult.Value,
+                video.ThumbnailsInterval);
+            if (framesResult.IsFailure)
+            {
+                logger.LogError("Erro ao extrair frames do vídeo com ID: {VideoId}. Motivo: {Error}", video.Id, framesResult.Error.Message);
+                return Result.Failure(framesResult.Error);
+            }
+
+            logger.LogInformation("Frames do vídeo extraídos com sucesso. ID: {VideoId}", video.Id);
+
+            var uploadResult = await storageService.UploadAsync(
+                video.Id,
+                framesResult.Value,
+                "application/x-zip-compressed",
+                cancellationToken);
+            if (uploadResult.IsFailure)
+            {
+                logger.LogError("Erro ao fazer upload dos frames do vídeo com ID: {VideoId}. Motivo: {Error}", video.Id, uploadResult.Error.Message);
+                return Result.Failure(uploadResult.Error);
+            }
+
+            logger.LogInformation("Upload dos frames realizado com sucesso. ID: {VideoId}", video.Id);
+
+            await videoRepository.ChangeStatusAsync(video.Id.ToString(), VideoStatus.Processed, cancellationToken);
+            logger.LogInformation("Status do vídeo alterado para 'Processado'. ID: {VideoId}", video.Id);
+
+            return Result.Success();
         }
-
-        // Baixar videos do blob
-        var videoDownloadResult = await videoDownloader.DownloadAsync(video.FilePath);
-
-        // Processar video estraindo frames
-        // Mudar status do video para processando
-        // Zipar os frames
-        // Enviar os frames para o blob
-        // Mudar status do video para processado
-
-
-        return Result.Success(new VideoProcessorResponse());
+        catch (Exception e)
+        {
+            logger.LogError(e, "Erro inesperado ao processar o vídeo com ID: {VideoId}", request.VideoId);
+            await videoRepository.ChangeStatusAsync(request.VideoId, VideoStatus.Failed, cancellationToken);
+            return Result.Failure(Error.Failure("VideoProcessorCommandHandler.Handle",
+                $"Erro ao processar o vídeo com ID {request.VideoId}: {e.Message}"));
+        }
     }
 }
