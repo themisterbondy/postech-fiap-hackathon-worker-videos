09bb569e20e6ee5e22cfe5718e2c5f39542d402d
Alexandre Nascimento <alexandre.n@outlook.com>
2025-04-12 18:26:10 -0300

    Refactor VideoMessageReceiver to use IMediator instead of ISender
    Replaced ISender with IMediator in VideoMessageReceiver for better alignment with the mediator pattern. Updated dependencies and method calls accordingly to ensure seamless message processing. This change enhances maintainability and consistency within the codebase.

--- src/Postech.Fiap.Hackathon.VideoProcessing.Worker/Features/Videos/VideoProcessor/Services/VideoMessageReceiver.cs
+++ src/Postech.Fiap.Hackathon.VideoProcessing.Worker/Features/Videos/VideoProcessor/Services/VideoMessageReceiver.cs
@@ -2,17 +2,18 @@ using Microsoft.Azure.Storage.Queue;
 using Postech.Fiap.Hackathon.VideoProcessing.Worker.Common.ResultPattern;
 using Postech.Fiap.Hackathon.VideoProcessing.Worker.Features.Videos.VideoProcessor.Commands;
 using Postech.Fiap.Hackathon.VideoProcessing.Worker.Features.Videos.VideoProcessor.Interfaces;
+using Postech.Fiap.Hackathon.VideoProcessing.Worker.Features.Videos.VideoProcessor.Notifications;
 
 namespace Postech.Fiap.Hackathon.VideoProcessing.Worker.Features.Videos.VideoProcessor.Services;
 
-public class VideoMessageReceiver(ISender sender, ILogger<VideoMessageReceiver> logger) : IMessageReceiver
+public class VideoMessageReceiver(IMediator mediator, ILogger<VideoMessageReceiver> logger) : IMessageReceiver
 {
     public async Task<Result> ReceiverAsync(CloudQueueMessage message)
     {
         var videoId = message.AsString;
         logger.LogInformation("Recebido vídeo: {VideoId}", videoId);
 
-        var processingResult = await sender.Send(new VideoProcessorCommand(videoId));
+        var processingResult = await mediator.Send(new VideoProcessorCommand(videoId));
         if (processingResult.IsFailure)
         {
             logger.LogError("Erro ao processar vídeo: {Error}", processingResult.Error);
