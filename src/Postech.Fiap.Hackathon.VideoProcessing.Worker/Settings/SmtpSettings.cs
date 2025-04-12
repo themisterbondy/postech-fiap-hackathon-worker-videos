2ecc3fd99d54ef2a36c9193f27432d48d91c10c4
Alexandre Nascimento <alexandre.n@outlook.com>
2025-04-12 18:26:23 -0300

    Add SmtpSettings class for email configuration
    Introduced a new SmtpSettings class to manage SMTP-related configurations such as host, port, SSL, and credentials. This enables centralized and structured handling of email configuration within the application.

---
+++ src/Postech.Fiap.Hackathon.VideoProcessing.Worker/Settings/SmtpSettings.cs
@@ -0,0 +1,15 @@
+using System.Security;
+
+namespace Postech.Fiap.Hackathon.VideoProcessing.Worker.Settings;
+
+public class SmtpSettings
+{
+    public string? Host { get; set; } = string.Empty;
+    public int Port { get; set; } = 0;
+    public bool EnableSsl { get; set; }
+
+    public string From { get; set; } = string.Empty;
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+
+}
